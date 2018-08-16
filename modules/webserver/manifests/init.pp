2b0ac43d92ffd51f4a70545e336beb27663934ee
puppettst3 <puppettst3@example.com>
2018-08-16 09:31:57 +0000

    Missing comma added in Service require section

--- modules/webserver/manifests/init.pp
+++ modules/webserver/manifests/init.pp
@@ -4,21 +4,21 @@ class webserver {
 	}
 
 	file { '/etc/httpd/conf/httpd.conf':
-		ensure => file,
-		source => 'puppet:///modules/webserver/httpd.conf',
+		ensure  => file,
+		source  => 'puppet:///modules/webserver/httpd.conf',
 		require => Package['httpd']
 	}
 
 	file { '/etc/httpd/conf.d/vhost.conf':
-		ensure => file,
+		ensure  => file,
 		content => template('webserver/vhost.conf.erb'),
 		require => Package['httpd']
 	}
 
 	service { 'httpd' :
-		ensure => running,
-		enable => true,
+		ensure     => running,
+		enable     => true,
 		hasrestart => true,
-		require => [ File[/etc/httpd/conf/httpd.conf'], File['/etc/httpd/conf.d/vhost.conf'] ] 
+		require    => [ File['/etc/httpd/conf/httpd.conf'], File['/etc/httpd/conf.d/vhost.conf'] ] 
 	}
 }
