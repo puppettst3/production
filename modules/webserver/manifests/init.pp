25016fd290000cdc8617ce3252cb33411911cb34
puppettst3 <puppettst3@example.com>
2018-08-16 09:29:34 +0000

    Adding the dependencies to apply proper updates to service

--- modules/webserver/manifests/init.pp
+++ modules/webserver/manifests/init.pp
@@ -5,17 +5,20 @@ class webserver {
 
 	file { '/etc/httpd/conf/httpd.conf':
 		ensure => file,
-		source => 'puppet:///modules/webserver/httpd.conf'
+		source => 'puppet:///modules/webserver/httpd.conf',
+		require => Package['httpd']
 	}
 
 	file { '/etc/httpd/conf.d/vhost.conf':
 		ensure => file,
-		content => template('webserver/vhost.conf.erb')
+		content => template('webserver/vhost.conf.erb'),
+		require => Package['httpd']
 	}
 
 	service { 'httpd' :
 		ensure => running,
 		enable => true,
-		hasrestart => true
+		hasrestart => true,
+		require => [ File[/etc/httpd/conf/httpd.conf'], File['/etc/httpd/conf.d/vhost.conf'] ] 
 	}
 }
