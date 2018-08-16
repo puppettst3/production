19443ee174ec2deaf62770b3bc0b9e1f2f5a1899
puppettst3 <puppettst3@example.com>
2018-08-16 09:18:25 +0000

    module updates with resource definition specific to centos agent

--- modules/webserver/manifests/init.pp
+++ modules/webserver/manifests/init.pp
@@ -1,13 +1,21 @@
 class webserver {
-	if $::osfamily == 'RedHat' {
-		package { 'httpd':
-			ensure => present
-		}	
-	} elsif $::osfamily == 'Debian' {
-		package { 'apache2':
-			ensure => present
-		}
-	} else {
-		notify { 'This distribution is not supported.' }
+	package { 'httpd':
+		ensure => present
+	}
+
+	file { '/etc/httpd/conf/httpd.conf':
+		ensure => file,
+		source => 'puppet:///modules/webserver/httpd.conf'
+	}
+
+	file { '/etc/httpd/conf.d/vhost.conf':
+		ensure => file,
+		content => template('webserver/vhost.conf.erb')
+	}
+
+	service { 'httpd' :
+		ensure => running,
+		enable => true,
+		hasrestart => true
 	}
 }
